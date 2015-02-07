5a9c63758dbaae17c40c972c2000e0fdb8ef4efb
mwpeterson <michael.peterson@gatewaypeople.com>
2015-02-07 09:17:29 -0600

    add aliases.drushrc.php

---
+++ aliases.drushrc.php
@@ -0,0 +1,17 @@
+<?php
+$aliases['gwayppl.prod.gwp'] = array(
+  'parent' => '@gwayppl.prod',
+  'uri' => 'gwp',
+);
+$aliases['gwayppl.test.gwp'] = array(
+  'parent' => '@gwayppl.test',
+  'uri' => 'gwp',
+);
+$aliases['gwayppl.dev.gwp'] = array(
+  'parent' => '@gwayppl.dev',
+  'uri' => 'gwp',
+);
+$aliases['gwayppl.ra.gwp'] = array(
+  'parent' => '@gwayppl.ra',
+  'uri' => 'gwp',
+);
